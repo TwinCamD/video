9a335f79bddeba6e9d436b677204262426f00743
TwinCamD <59870277+TwinCamD@users.noreply.github.com>
2020-02-18 22:15:22 +0900

    Update script.js

--- script.js
+++ script.js
@@ -91,7 +91,7 @@ function getpeerid(id) {
 
     //peerオブジェクトの作成
     peer = new Peer(id,{
-        key: '6cee6718-08d3-4ce7-93a9-237ecd4601bb',     //APIkey
+        key: 'bdd396a0-1b22-4921-824b-4582ad17e739',     //APIkey
         debug: 3
     });
 
